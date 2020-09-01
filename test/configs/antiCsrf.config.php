c980dd83f7654bb169a242313a3bbac11bad8821
danielaltamirano1993 <64813513+danielaltamirano1993@users.noreply.github.com>
2020-09-01 12:01:10 -0500

    Create antiCsrf.config.php

---
+++ test/configs/antiCsrf.config.php
@@ -0,0 +1,15 @@
+<?php
+/*
+ * This file is part of Aplus Framework MVC Library.
+ *
+ * (c) Natan Felles <natanfelles@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+return [
+    'default' => [
+        'enabled' => false,
+        'token_name' => 'csrf_token',
+    ],
+];
