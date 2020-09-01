762b3e40605c06f7209538afd7dddf6edb2906c0
danielaltamirano1993 <64813513+danielaltamirano1993@users.noreply.github.com>
2020-09-01 12:01:02 -0500

    Create autoloader.config.php

---
+++ test/configs/autoloader.config.php
@@ -0,0 +1,19 @@
+<?php
+/*
+ * This file is part of Aplus Framework MVC Library.
+ *
+ * (c) Natan Felles <natanfelles@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+return [
+    'default' => [
+        'register' => true,
+        'extensions' => '.php',
+        'classes' => [],
+        'namespaces' => [
+            'Tests\\MVC' => dirname(__DIR__),
+        ],
+    ],
+];
