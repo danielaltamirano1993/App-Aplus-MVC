4461be45c99b8e82559d926221e3a48590346be7
danielaltamirano1993 <64813513+danielaltamirano1993@users.noreply.github.com>
2020-09-07 12:47:33 -0500

    Create validation.php

---
+++ src/Languages/pt-br/validation.php
@@ -0,0 +1,13 @@
+<?php
+/*
+ * This file is part of Aplus Framework MVC Library.
+ *
+ * (c) Natan Felles <natanfelles@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+return [
+    'notUnique' => 'O campo {field} não está registrado.',
+    'unique' => 'O campo {field} já foi registrado.',
+];
