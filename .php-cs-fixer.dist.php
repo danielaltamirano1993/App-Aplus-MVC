0c6d55f6ebd2963170ef1240ae0fbb69ce8a3316
danielaltamirano1993 <64813513+danielaltamirano1993@users.noreply.github.com>
2022-10-09 11:50:59 -0500

    Create .php-cs-fixer.dist.php

---
+++ .php-cs-fixer.dist.php
@@ -0,0 +1,18 @@
+<?php declare(strict_types=1);
+/*
+ * This file is part of Aplus Framework MVC Library.
+ *
+ * (c) Natan Felles <natanfelles@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+use Framework\CodingStandard\Config;
+use Framework\CodingStandard\Finder;
+
+return (new Config())->setDefaultHeaderComment(
+    'Aplus Framework MVC Library',
+    'Natan Felles <natanfelles@gmail.com>'
+)->setFinder(
+    Finder::create()->in(__DIR__)
+);
