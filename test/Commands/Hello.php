4f3a56fdc14d2fe1ab2e153f6c5a883b5e3e207b
danielaltamirano1993 <64813513+danielaltamirano1993@users.noreply.github.com>
2022-10-10 11:55:56 -0500

    Create Hello.php

---
+++ test/Commands/Hello.php
@@ -0,0 +1,22 @@
+<?php
+/*
+ * This file is part of Aplus Framework MVC Library.
+ *
+ * (c) Natan Felles <natanfelles@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+namespace Tests\MVC\Commands;
+
+use Framework\CLI\Command;
+
+class Hello extends Command
+{
+    protected string $name = 'hello';
+    protected string $description = 'Say hello.';
+
+    public function run() : void
+    {
+    }
+}
