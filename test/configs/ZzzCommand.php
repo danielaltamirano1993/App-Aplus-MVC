7eadbfc93c4f244449c677a73bebb8a4814e8f17
danielaltamirano1993 <64813513+danielaltamirano1993@users.noreply.github.com>
2022-10-11 11:56:52 -0500

    Create ZzzCommand.php

---
+++ test/configs/ZzzCommand.php
@@ -0,0 +1,17 @@
+<?php
+/*
+ * This file is part of Aplus Framework MVC Library.
+ *
+ * (c) Natan Felles <natanfelles@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+use Framework\CLI\Command;
+
+class ZzzCommand extends Command
+{
+    public function run() : void
+    {
+    }
+}
