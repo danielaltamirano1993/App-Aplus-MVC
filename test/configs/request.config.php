8c389241bbea6d58248f35212e82cf80b688c8b3
danielaltamirano1993 <64813513+danielaltamirano1993@users.noreply.github.com>
2022-10-11 11:57:57 -0500

    Create request.config.php

---
+++ test/configs/request.config.php
@@ -0,0 +1,18 @@
+<?php
+/*
+ * This file is part of Aplus Framework MVC Library.
+ *
+ * (c) Natan Felles <natanfelles@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+return [
+    'default' => [
+        'allowed_hosts' => [],
+        'force_https' => false,
+        'server_vars' => [
+            'HTTP_FOO' => 'Foo',
+        ],
+    ],
+];
