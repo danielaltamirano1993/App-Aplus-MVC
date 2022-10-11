c28deab953434f867091456791c94e527d0abba1
danielaltamirano1993 <64813513+danielaltamirano1993@users.noreply.github.com>
2022-10-11 11:57:01 -0500

    Create view.config.php

---
+++ test/configs/view.config.php
@@ -0,0 +1,17 @@
+<?php
+/*
+ * This file is part of Aplus Framework MVC Library.
+ *
+ * (c) Natan Felles <natanfelles@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+return [
+    'default' => [
+        'base_dir' => __DIR__ . '/../Views',
+        'extension' => '.php',
+        'layout_prefix' => '',
+        'include_prefix' => '',
+    ],
+];
